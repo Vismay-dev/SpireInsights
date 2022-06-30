5de8b27402469bc824e07a6bb3289a5efa26af48
Vismay-dev <vismaysuramwar@gmail.com>
2022-06-30 15:37:01 +0400

    Initial commit

--- app.js
+++ app.js
@@ -30,6 +30,16 @@ app.use('/api/user',RoutesAPIUser)
 
 const port = process.env.PORT||4000;
 
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.use('*', express.static('client/build'));     
+
+    app.get('*', (req,res)=> {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 app.listen(port,()=> {
     console.log(`- Successfully connected to server at port ${port}`)
 })
