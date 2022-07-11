ff9e995092e3657b55f66daa55f7330eb69c0a77
Vismay-dev <vismaysuramwar@gmail.com>
2022-07-11 17:39:44 +0400

    Wtv

--- client/src/MainContent/Components/home/Pricing.js
+++ client/src/MainContent/Components/home/Pricing.js
@@ -4,6 +4,7 @@ import Tooltip from 'react-power-tooltip'
 const Pricing = () => {
 
    const [showToolTip, setShowToolTip] = useState(false)
+   const [showToolTip2, setShowToolTip2] = useState(false)
 
 return (
 <section
@@ -11,8 +12,8 @@ return (
    bg-gradient-to-br from-blue-50 to-indigo-50
    pt-[74px]
    lg:pt-[66px]
-   pb-10
-   lg:pb-[47px]
+   pb-8
+   lg:pb-[43px]
    relative
    z-20
    overflow-hidden
@@ -44,8 +45,8 @@ return (
             </div>
          </div>
       </div>
-      <div class="flex flex-wrap justify-center lg:w-[80%] w-[95%] mx-auto relative ">
-         <div class="w-full md:w-1/2 lg:px-4 px-1">
+      <div class="flex flex-wrap justify-center lg:w-[98%] md:w-[97%] sm:w-[80%] w-[90%] mx-auto relative ">
+         <div class="w-full lg:w-1/3 md:w-1/2 md:px-4 px-1">
             <div
                class="
                bg-white
@@ -70,7 +71,7 @@ return (
                <h2 class="font-bold text-dark mb-5  text-[42px]">
                   Free Trial
                   <span class="text-base m-1 text-body-color font-medium">
-                  / 48 <span class = 'lg:inline hidden'>hours</span> <span class = 'lg:hidden inline'>hrs</span>
+                  / 48 <span class = 'lg:inline hidden'>hrs</span> <span class = 'lg:hidden inline'>hrs</span>
                   </span>
                </h2>
                <p
@@ -83,8 +84,7 @@ return (
 
                   "
                   >
-                  Perfect for testing out our services.
-               </p>
+Test out our services.               </p>
                <p
                   class="
                   text-base text-body-color
@@ -101,7 +101,7 @@ return (
                      48 Hours
                   </p>
                   <p class="text-base text-body-color leading-loose mb-1">
-                     All Features & Services
+                     Limited Features & Services
                   </p>
                   <p class="text-base text-body-color leading-loose mb-1">
                      Limited Access
@@ -111,6 +111,9 @@ return (
                   </p>
                   <p class="text-base text-body-color leading-loose mb-1">
                      Test out Optimization Algorithms
+                  </p>
+                  <p class="text-base text-body-color leading-loose mb-1">
+                     No Live Queries
                   </p>
                   <p class="text-base text-body-color leading-loose mb-1">
                      Customer Support
@@ -129,6 +132,8 @@ return (
                   rounded-md
                   text-center
                   p-4
+                  -mt-[3px]
+                  -mb-[1px]
                   hover:bg-blue-700 hover:text-white hover:shadow-md
                   transition
                   "
@@ -401,7 +406,7 @@ return (
                </div>
             </div>
          </div>
-         <div class="w-full md:w-1/2 lg:px-4 px-1">
+         <div class="w-full lg:w-1/3 md:w-1/2 md:px-4 px-1">
             <div
                class="
                bg-white
@@ -439,7 +444,7 @@ return (
                   border-b border-[#F2F2F2]
                   "
                   >
-                  Perfect for use in a Business setting.
+                  Paid Features & Unlimited Access.
                </p>
 
                <p
@@ -461,7 +466,380 @@ return (
                     All Features & Services
                   </p>
                   <p class="text-base text-body-color leading-loose mb-1">
-                  Real-time Insights on Marketplaces.
+                  Real-time Insights <span class = 'inline'>on Marketplace trends.</span>
+                  </p>
+                  <p class="text-base text-body-color leading-loose mb-1">
+                     Synchronized with Your <span class = 'hidden lg:inline'>Marketplace</span> Listing 
+                  </p>
+                  <p class="text-base text-body-color leading-loose mb-1">
+                     Unrestricted Usage of Analytics.
+                  </p>
+                  <p class="text-base lg:hidden block text-body-color leading-loose mb-1">
+                     Live Queries
+                  </p>
+                  <p class="text-base text-body-color leading-loose mb-1">
+                     Customer Support
+                  </p>
+               </div>
+            
+               <a
+                onMouseOver={() => {
+                  setShowToolTip2(true)}}
+                onMouseLeave={() => setShowToolTip2(false)}
+                  class="
+                  w-full
+                  block
+                  text-base
+                  font-semibold
+                  text-gray-700
+                  bg-primary
+                  border border-primary
+                  rounded-md
+                  text-center
+                  p-4
+                  transition
+                  bg-gray-200
+                  "
+                  >
+               Choose Business
+               </a>
+        
+               <div>
+                  <span class="absolute right-0 top-7 z-[-1]">
+                     <svg
+                        width="77"
+                        height="172"
+                        viewBox="0 0 77 172"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        >
+                        <circle cx="86" cy="86" r="86" fill="url(#paint0_linear)" />
+                        <defs>
+                           <linearGradient
+                              id="paint0_linear"
+                              x1="86"
+                              y1="0"
+                              x2="86"
+                              y2="172"
+                              gradientUnits="userSpaceOnUse"
+                              >
+                              <stop stop-color="#3056D3" stop-opacity="0.09" />
+                              <stop
+                                 offset="1"
+                                 stop-color="#C4C4C4"
+                                 stop-opacity="0"
+                                 />
+                           </linearGradient>
+                        </defs>
+                     </svg>
+                  </span>
+                  <span class="absolute right-4 top-4 z-[-1]">
+                     <svg
+                        width="41"
+                        height="89"
+                        viewBox="0 0 41 89"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        >
+                        <circle
+                           cx="38.9138"
+                           cy="87.4849"
+                           r="1.42021"
+                           transform="rotate(180 38.9138 87.4849)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="38.9138"
+                           cy="74.9871"
+                           r="1.42021"
+                           transform="rotate(180 38.9138 74.9871)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="38.9138"
+                           cy="62.4892"
+                           r="1.42021"
+                           transform="rotate(180 38.9138 62.4892)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="38.9138"
+                           cy="38.3457"
+                           r="1.42021"
+                           transform="rotate(180 38.9138 38.3457)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="38.9138"
+                           cy="13.634"
+                           r="1.42021"
+                           transform="rotate(180 38.9138 13.634)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="38.9138"
+                           cy="50.2754"
+                           r="1.42021"
+                           transform="rotate(180 38.9138 50.2754)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="38.9138"
+                           cy="26.1319"
+                           r="1.42021"
+                           transform="rotate(180 38.9138 26.1319)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="38.9138"
+                           cy="1.42021"
+                           r="1.42021"
+                           transform="rotate(180 38.9138 1.42021)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="26.4157"
+                           cy="87.4849"
+                           r="1.42021"
+                           transform="rotate(180 26.4157 87.4849)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="26.4157"
+                           cy="74.9871"
+                           r="1.42021"
+                           transform="rotate(180 26.4157 74.9871)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="26.4157"
+                           cy="62.4892"
+                           r="1.42021"
+                           transform="rotate(180 26.4157 62.4892)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="26.4157"
+                           cy="38.3457"
+                           r="1.42021"
+                           transform="rotate(180 26.4157 38.3457)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="26.4157"
+                           cy="13.634"
+                           r="1.42021"
+                           transform="rotate(180 26.4157 13.634)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="26.4157"
+                           cy="50.2754"
+                           r="1.42021"
+                           transform="rotate(180 26.4157 50.2754)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="26.4157"
+                           cy="26.1319"
+                           r="1.42021"
+                           transform="rotate(180 26.4157 26.1319)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="26.4157"
+                           cy="1.4202"
+                           r="1.42021"
+                           transform="rotate(180 26.4157 1.4202)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="13.9177"
+                           cy="87.4849"
+                           r="1.42021"
+                           transform="rotate(180 13.9177 87.4849)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="13.9177"
+                           cy="74.9871"
+                           r="1.42021"
+                           transform="rotate(180 13.9177 74.9871)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="13.9177"
+                           cy="62.4892"
+                           r="1.42021"
+                           transform="rotate(180 13.9177 62.4892)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="13.9177"
+                           cy="38.3457"
+                           r="1.42021"
+                           transform="rotate(180 13.9177 38.3457)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="13.9177"
+                           cy="13.634"
+                           r="1.42021"
+                           transform="rotate(180 13.9177 13.634)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="13.9177"
+                           cy="50.2754"
+                           r="1.42021"
+                           transform="rotate(180 13.9177 50.2754)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="13.9177"
+                           cy="26.1319"
+                           r="1.42021"
+                           transform="rotate(180 13.9177 26.1319)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="13.9177"
+                           cy="1.42019"
+                           r="1.42021"
+                           transform="rotate(180 13.9177 1.42019)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="1.41963"
+                           cy="87.4849"
+                           r="1.42021"
+                           transform="rotate(180 1.41963 87.4849)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="1.41963"
+                           cy="74.9871"
+                           r="1.42021"
+                           transform="rotate(180 1.41963 74.9871)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="1.41963"
+                           cy="62.4892"
+                           r="1.42021"
+                           transform="rotate(180 1.41963 62.4892)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="1.41963"
+                           cy="38.3457"
+                           r="1.42021"
+                           transform="rotate(180 1.41963 38.3457)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="1.41963"
+                           cy="13.634"
+                           r="1.42021"
+                           transform="rotate(180 1.41963 13.634)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="1.41963"
+                           cy="50.2754"
+                           r="1.42021"
+                           transform="rotate(180 1.41963 50.2754)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="1.41963"
+                           cy="26.1319"
+                           r="1.42021"
+                           transform="rotate(180 1.41963 26.1319)"
+                           fill="#3056D3"
+                           />
+                        <circle
+                           cx="1.41963"
+                           cy="1.4202"
+                           r="1.42021"
+                           transform="rotate(180 1.41963 1.4202)"
+                           fill="#3056D3"
+                           />
+                     </svg>
+                  </span>
+               </div>
+            </div>
+
+            <div class = 'block relative right-[55%] bottom-16'>
+               <Tooltip show={showToolTip2} position = 'bottom' fontSize = '16px' padding = '3px 5px'>
+  <span class = 'font-semibold text-center font-sans bottom-0.5'>Paid version will be available soon...</span>
+</Tooltip>
+</div>
+
+         </div>
+
+
+         <div class="w-full lg:w-1/3 md:w-1/2 md:px-4 px-1">
+            <div
+               class="
+               bg-white
+               shadow-lg
+
+               rounded-xl
+               relative
+               z-10
+               overflow-hidden
+               border border-primary border-opacity-20
+               shadow-pricing
+               py-10
+               px-8
+               sm:p-12
+               lg:py-10 lg:px-6
+               xl:p-12
+               mb-10
+               "
+               >
+               <span class="text-primary font-semibold text-lg block mb-4">
+               Business
+               </span>
+               <h2 class="font-bold text-dark mb-5 text-[42px]">
+                  $50
+                  <span class="text-base text-body-color font-medium">
+                  / 3 months
+                  </span>
+               </h2>
+               <p
+                  class="
+                  text-base text-body-color
+                  pb-8
+                  mb-6
+                  lg:block hidden
+                  border-b border-[#F2F2F2]
+                  "
+                  >
+                  <span class = 'font-bold text-indigo-600 mr-[2px] text-2xl'>20% Discount.</span> 
+               </p>
+
+               <p
+                  class="
+                  text-base text-body-color
+                  pb-8
+                  mb-8
+                  border-b border-[#F2F2F2]
+                  block lg:hidden
+                  "
+                  >
+                  20% Discount.
+               </p>
+               <div class="mb-7">
+                  <p class="text-base text-body-color leading-loose mb-1">
+                     Unlimited Access
+                  </p>
+                  <p class="text-base text-body-color leading-loose mb-1">
+                    All Features & Services
+                  </p>
+                  <p class="text-base text-body-color leading-loose mb-1">
+                  Real-time Insights on Marketplace trends.
                   </p>
                   <p class="text-base text-body-color leading-loose mb-1">
                      Synchronized with Your <span class = 'hidden lg:inline'>Marketplace</span> Listing 
@@ -769,6 +1147,7 @@ return (
 </div>
 
          </div>
+      
       
         
       </div>
