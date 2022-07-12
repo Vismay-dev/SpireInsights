15be579569a42304d8a8f4fa12ba5c860a58c95a
Vismay-dev <vismaysuramwar@gmail.com>
2022-07-12 12:33:57 +0400

    Wtv

--- server/routes/RoutesAPIUser.js
+++ server/routes/RoutesAPIUser.js
@@ -38,8 +38,8 @@ port: 465,
                 clientId: process.env.CLIENT_ID,
                 clientSecret:process.env.CLIENT_SECRET,
                 refreshToken: process.env.REFRESH_TOKEN,
-                accessToken: process.env.ACCESS_TOKEN
-                // expires: 3600
+                accessToken: process.env.ACCESS_TOKEN,
+                expires: 3600
             } 
         })
     
