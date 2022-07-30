7bf9e3c058fc5a5710ad5dbacde81181a4ece720
Vismay-dev <vismaysuramwar@gmail.com>
2022-07-30 12:01:22 +0400

    Commit 2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      apps: [
+        {
+          name: "app",
+          script: "./app.js",
+          instances: "max",
+          env: {
+            NODE_ENV: "development",
+          },
+          env_production: {
+            NODE_ENV: "production",
+          },
+        },
+      ],
+    },
+  ],
+};
