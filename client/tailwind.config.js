af0c7545163d99779c15b3e60e4a1c43967c66b4
Vismay-dev <vismaysuramwar@gmail.com>
2022-11-14 08:00:11 +0400

    Ye

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,28 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        bermuda: "#78dcca",
+        gold: "#f59e0b",
+      },
+      screens: {
+        custombp: { raw: "(max-height: 540px)" },
+
+        xs: "530px",
+        xs2: "480px",
+        xs3: "425px",
+        xs4: "380px",
+      },
+    },
   },
-  plugins: [],
-}
+};
---
+++ server/routes/RoutesSP_API.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const auth = require("../auth/auth");
+
+module.exports = router;
